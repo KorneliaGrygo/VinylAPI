3e7b0d9e2e0b3dd676962ef7b1418ebcac60bc55
Kornelia <grygo96@o2.pl>
2021-12-09 16:59:39 +0100

    Przygotowanie pod logowanie / rejestracje

---
+++ VinylAPI/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VinylAPI.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult RegisterUser([FromBody]RegisterUserDto dto)
+        {
+
+            
+        }
+    }
+}
--- VinylAPI/VinylSeeder.cs
+++ VinylAPI/VinylSeeder.cs
@@ -25,7 +25,30 @@ namespace VinylAPI
                     _dbContext.Bands.AddRange(bands);
                     _dbContext.SaveChanges();
                 }
+
+                if(!_dbContext.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Roles.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
             }
+        }
+
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role()
+                {
+                    Name = "User"
+                },
+                new Role()
+                {
+                    Name = "Admin"
+                }
+            };
+            return roles;
         }
 
         private IEnumerable<Band> GetBands()
