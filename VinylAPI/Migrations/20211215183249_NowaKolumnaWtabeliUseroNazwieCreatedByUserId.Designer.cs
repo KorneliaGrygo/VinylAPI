e74c81b0ccde65818cb151c8432dd6404bdc3ee4
Kornelia <grygo96@o2.pl>
2021-12-15 20:17:53 +0100

    Migracje na bazie danych

--- VinylAPI/Migrations/20211215183249_NowaKolumnaWtabeliUseroNazwieCreatedByUserId.Designer.cs
+++ VinylAPI/Migrations/20211215183249_NowaKolumnaWtabeliUseroNazwieCreatedByUserId.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VinylAPI.Data;
 
 namespace VinylAPI.Migrations
 {
     [DbContext(typeof(VinylAPIDbContext))]
-    partial class VinylAPIDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211215183249_NowaKolumnaWtabeliUseroNazwieCreatedByUserId")]
+    partial class NowaKolumnaWtabeliUseroNazwieCreatedByUserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VinylAPI/Migrations/20211215183249_NowaKolumnaWtabeliUseroNazwieCreatedByUserId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VinylAPI.Migrations
+{
+    public partial class NowaKolumnaWtabeliUseroNazwieCreatedByUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedByUserId",
+                table: "Bands",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "Bands");
+        }
+    }
+}
