21615c7bcb5bec78d9eccbd074b7879dc66d3837
Kornelia <grygo96@o2.pl>
2021-12-15 20:18:53 +0100

    klasa query bazowa do filtrowania

--- VinylAPI/Models/Query.cs
+++ VinylAPI/Models/Query.cs
@@ -1,9 +1,20 @@
 ﻿namespace VinylAPI.Models
 {
-    public class Query
+
+    public class BandQuery : Query
+    {
+
+    }
+    public class MusicAlbumQuery : Query
+    {
+
+    }
+    public class SongQuery : Query { 
+    }
+    public abstract class Query
     {
         public string SearchPhrase { get; set; }
-        public int PageSize { get; set; }
+        public int PageSize { get; set; } = 20;
         public int PageNumber { get; set; }
         public string SortBy { get; set; }
         public SortDirection SortDirection { get; set; }
